c5e3e618abdcac9c3cebbb3be9a4d1d4891d3d30
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 12:59:23 -0300

    Aula de array

---
+++ Array/script.js
@@ -0,0 +1,49 @@
+//const nomes = ['Caio', 15 , 'Fiap' , {}]
+
+ //atualizar dados
+ //nomes[1] = 'Vitor'
+
+
+ //adicionar dados
+ //nomes.push('Daniel')
+
+ //Excluir valor 
+ //nomes.pop()
+
+//console.log(nomes)
+
+
+const times = ['Corinthians' , 'Serie B' , 'Trikas' , 'Palmeiras' ]
+
+//for(let i=0; i < times.length ; i+=2){
+
+   // console.log(times[i])
+//}
+
+//ForEach ele verifica todos os elementos
+
+//times.forEach(function(meuTime){
+    //console.log(meuTime)
+//})
+
+
+//times.forEach(Elements=>{
+    //console.log(Elements)
+//})
+
+
+//Map ele devolve um novo array
+
+//const timesModificados = times.map(elements =>{
+    //console.log(`Meu time é : ${elements}`)
+//})
+
+
+// filtrar elemento
+
+
+//console.log(times.filter(Elements =>{
+    //return Elements === 'Corinthians'
+//}))
+
+
